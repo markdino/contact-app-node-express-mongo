86cec75e2ac8a75c6ffb4b82e028bd9fd042912c
Mark Dino <markdinopelonia447@gmail.com>
2020-02-20 10:31:20 +0800

    feat: Render index.ejs to show list of contact in Home route

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const app = express();
+const jsonData = require("./data.json");
+
+app.set("view engine", "ejs");
+app.use(express.static("./assets"));
+
+const data = jsonData;
 
 app.get("/", (req, res) => {
-  res.send("Home route");
+  data.sort(function(a, b) {
+    const nameA = a.name.toUpperCase();
+    const nameB = b.name.toUpperCase();
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+    return 0;
+  });
+  res.render("index", { data });
 });
 
 app.listen(3000, () => {
