a4188b173a3804cde4fde734a3a3694668d24975
Mark Dino <markdinopelonia447@gmail.com>
2020-05-26 21:14:25 +0800

    feat: Create a function (search, payload, & filter )  for API

--- models/contact.js
+++ models/contact.js
@@ -13,23 +13,44 @@ const personSchema = new mongoose.Schema({
 
 module.exports = mongoose.model("contact", personSchema);
 
-module.exports.search = (result, req, res, options) => {
-  const searched = result.filter(person => {
+module.exports.search = (result, query, req, res, options) => {
+  let searched = result.filter(person => {
     const personLC = person.name.toLowerCase();
-    const searchLC = req.body.search.toLowerCase();
+    const searchLC = query.toLowerCase();
     return personLC.includes(searchLC);
   });
-  if (req.body.search === "") {
-    return res.redirect(options.emptyRedirect);
-  } else {
-    return res.render(options.render, {
-      data: searched,
-      user: req.user,
-      status: options.status
-    });
+  if (searched.length <= 0) searched = `No contact matched for "${query}"`;
+
+  if (!req) { return searched }
+  else {
+    if (query === "") {
+      return res.redirect(options.emptyRedirect);
+    } else {
+      return res.render(options.render, {
+        data: searched,
+        user: req.user,
+        status: options.status
+      });
+    }
   }
+
 };
 
 module.exports.errorAlert = (code, status, message) => {
   return { Error: { code, status, message } };
 };
+
+module.exports.payload = (error, value, name) => {
+  return { error, value, name };
+};
+
+module.exports.filter = (obj, query) => {
+  const q = query.split(" ");
+  const newObj = {};
+
+  q.forEach(que => {
+    if (obj[que] || obj[que] === false) newObj[que] = obj[que];
+  })
+
+  return newObj
+}
