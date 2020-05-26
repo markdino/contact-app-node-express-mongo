ad84841e91a879b6ecd9c4bc92a9383546fd3b04
Mark Dino <markdinopelonia447@gmail.com>
2020-05-26 21:18:14 +0800

    feat: Use middleware express.json()

--- startups/middleware.js
+++ startups/middleware.js
@@ -4,5 +4,6 @@ const passport = require('../middleware/passportSetup')
 module.exports = app => {
     app.set("view engine", "ejs");
     app.use(express.static("./assets"));
-    passport.setup(app)
+    passport.setup(app);
+    app.use(express.json());
 }
