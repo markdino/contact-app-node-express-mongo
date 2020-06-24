1b77a311103feda31aac199628e50b039d5b2c63
Mark Dino <markdinopelonia447@gmail.com>
2020-06-24 18:22:07 +0800

    core: use cors as middleware

--- startups/middleware.js
+++ startups/middleware.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const passport = require('../middleware/passportSetup')
+const cors = require("cors");
 
 module.exports = app => {
     app.set("view engine", "ejs");
     app.use(express.static("./assets"));
+    app.use(cors());
     passport.setup(app);
     app.use(express.json());
 }
