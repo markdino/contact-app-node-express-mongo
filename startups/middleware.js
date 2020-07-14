f1867156de85006f17b4684d2a9b4c793450b522
Mark Dino <markdinopelonia447@gmail.com>
2020-07-14 14:30:33 +0800

    fix: Move CORS setup in separate file & get CORS whitelist from env

--- startups/middleware.js
+++ startups/middleware.js
@@ -1,14 +1,11 @@
 const express = require("express");
 const passport = require('../middleware/passportSetup')
-const cors = require("cors");
+const cors = require("../middleware/corsSetup");
 
 module.exports = app => {
     app.set("view engine", "ejs");
     app.use(express.static("./assets"));
     passport.setup(app);
     app.use(express.json());
-    app.use(cors({
-        origin: "http://localhost:3000",
-        credentials: true,
-    }));
+    cors.setup(app);
 }
